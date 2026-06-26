2a7e3e53c7e664063dbc51dfefd75bddf3124b44
Coren <coren114@gmail.com>
2026-06-26 15:12:34 -0500

    chore: add tests

--- backend/notes/admin.py
+++ backend/notes/admin.py
@@ -7,8 +7,8 @@ from notes.models import Note
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display: ClassVar = ["title", "user", "category", "created_at", "updated_at"]
+    list_display: ClassVar = ["title", "category", "created_at", "updated_at"]
     list_filter: ClassVar = ["category", "created_at"]
-    search_fields: ClassVar = ["title", "user__email"]
-    raw_id_fields: ClassVar = ["user", "category"]
+    search_fields: ClassVar = ["title", "category__name"]
+    raw_id_fields: ClassVar = ["category"]
     readonly_fields: ClassVar = ["created_at", "updated_at"]
